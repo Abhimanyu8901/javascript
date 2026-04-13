6cd364edcf70195c746547acd34e0ea8ed27e6d6
unknown <abhimanyu07052072434@email.in>
2026-04-13 20:29:48 +0530

    add content on home page

---
+++ project/files/includes/data.php
@@ -0,0 +1,14 @@
+<?php
+$siteName = 'Blood Saathi';
+$siteTagline = 'Companion in Time of Need';
+$primaryPhone = '+977-1-4001020';
+$emergencyPhone = '+977-9800000102';
+$supportEmail = 'support@bloodsaathi.org';
+$address = 'New Delhi, India';
+
+$partners = [
+    ['name' => 'National Red Cross Center', 'label' => 'Verified blood banking partner'],
+    ['name' => 'Delhi Hospital', 'label' => '24/7 emergency coordination partner'],
+    ['name' => 'Metro Lifeline Clinic', 'label' => 'Donor screening and triage support'],
+];
+?>
